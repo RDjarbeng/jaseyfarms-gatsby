132b79183b70527bfc9253313422e1f3c945de04
RDjarbeng <richarddjarbeng@gmail.com>
2020-12-04 23:18:58 +0000

    resolve merge conflict 2

--- src/components/Home/BenefitCard.js
+++ src/components/Home/BenefitCard.js
@@ -1,15 +1,30 @@
 import React from "react"
-
+import content from "../../content/BenefitsData.json"
+// {
+//   content.map(value => (
+//     <BenefitCard text={value} />
+//   ))
+// }
 export default function BenefitCard({ text }) {
   return (
     // benefit cards
     <React.Fragment>
+      <div className="row mt-2 mb-3 px-1 benefits-section">
       <div className="row mt-4">
         <div className="col-md-12 table-responsive">
           <div className="benefits-table" width="100%" padding="5px">
             <div className="align-middle">
-              <div className="row">
-                <div className="col-md-3 mb-3">
+                <div className="row">
+                  {
+                    content.map(value => (
+                      <div className="col-md-3 mb-3 box-marker">
+                        <div className="col benefit-box p-2  rounded shadow text-center vert-align">
+                          <p>{value}</p>
+                        </div>
+                      </div>
+                    ))
+                  }
+                {/* <div className="col-md-3 mb-3">
                   <div className="col benefit-box p-2 bg-success text-white rounded shadow text-center vert-align">
                     <p>Lower the risk of heart disease and stroke</p>
                   </div>
@@ -54,10 +69,11 @@ export default function BenefitCard({ text }) {
                       cataracts and macular degeneration
                     </p>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </React.Fragment>
--- src/components/Home/BenefitSection.js
+++ src/components/Home/BenefitSection.js
@@ -1,7 +1,7 @@
 import React from "react"
 import SectionTitle from "./SectionTitle"
 import BenefitCard from "./BenefitCard"
-import content from "../../content/BenefitsData.json"
+
 
 export default function ProductBenefits() {
   return (
@@ -11,12 +11,7 @@ export default function ProductBenefits() {
           title="BENEFITS"
           subtitle="A diet rich in vegetables can:"
         />
-        <div className="row mt-2 mb-3 px-1 benefits-section">
-          {/* {content.map(value => (
-            <BenefitCard text={value} />
-          ))} */}
           <BenefitCard/>
-        </div>
       </div>
     </section>
   )
